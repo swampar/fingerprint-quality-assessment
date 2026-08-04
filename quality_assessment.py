3ee82d09c14cca1a49ce19a0e43e67ed6ca04a83
sudeepa <sudeepm831@gmail.com>
2026-08-04 19:16:17 +0530

    Step 4: Add brightness detection

--- quality_assessment.py
+++ quality_assessment.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 def check_blur(image_path):
     image = cv2.imread(image_path)
 
@@ -19,4 +20,29 @@ def check_blur(image_path):
         print("✅ Image is Sharp")
 
 
+def check_brightness(image_path):
+    image = cv2.imread(image_path)
+
+    if image is None:
+        print("Image not found!")
+        return
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    brightness = gray.mean()
+
+    print(f"Brightness Score: {brightness:.2f}")
+
+    if brightness < 50:
+        print("❌ Image is Too Dark")
+
+    elif brightness > 210:
+        print("❌ Image is Too Bright")
+
+    else:
+        print("✅ Brightness is Good")
+
+
 check_blur("images/sample.jpg")
+print("----------------")
+check_brightness("images/sample.jpg")
